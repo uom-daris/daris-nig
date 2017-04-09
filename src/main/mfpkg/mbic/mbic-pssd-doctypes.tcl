d61d2d7af5abeee7cd5781bd2322618d6613b75b
Wilson Liu <wliu5@unimelb.edu.au>
2017-04-09 23:20:28 +1000

    added new doc types and method for ms gait project

--- src/main/mfpkg/mbic/mbic-pssd-doctypes.tcl
+++ src/main/mfpkg/mbic/mbic-pssd-doctypes.tcl
@@ -21,3 +21,23 @@ asset.doc.type.update :create yes :type nig-daris:pssd-mbic-fmp-check \
       :description "Has the SR DataSet been processed (by nig.pssd.mbic.dose.upload). " \
     > \
    >
+
+
+# Document nig-daris:ms-gait-subject
+asset.doc.type.update :create yes :type nig-daris:ms-gait-subject \
+    :label "nig-daris:ms-gait-subject" \
+    :description "Document for MS Gait subject information." \
+    :definition < \
+        :element -name address -label "Address" -type string -encrypt true -min-occurs 0 -max-occurs 1 < \
+            :description "The address of the subject." > \
+        :element -name phone -label "Phone Number" -type string -encrypt true -min-occurs 0 -max-occurs 3  < \
+            :description "The subject's phone number." > \
+        :element -name imed -label "IMED Number" -type string -encrypt true -min-occurs 0 -max-occurs 1 < \
+            :description "The subject's IMED number." > \
+        :element -name ur -label "UR Number" -type string -encrypt true -min-occurs 0 -max-occurs 1 < \
+            :description "The subject's UR number." > \
+        :element -name msg-code -label "MSG Code" -type string -encrypt true -min-occurs 0 -max-occurs 1 < \
+            :description "The subject's MSG code (MRI)." > \
+        :element -name ms-code -label "MS Code" -type string -encrypt true -min-occurs 0 -max-occurs 1 < \
+            :description "The subject's MS code (Gait Lab)." > >
+
